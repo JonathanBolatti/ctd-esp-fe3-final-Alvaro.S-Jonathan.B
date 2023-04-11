b0a9f1aba100b3e4ab14ac9b192643d0233ed019
Jonathan_Bolatti <jonathanbolatti@gmail.com>
2023-04-10 22:53:12 -0300

    se arregla footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,14 +3,8 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer style={{ margin_top: "200px"}}>
-        <p className='text_footer'>Powered by Alvaro Sanchez && Jonathan Bolatti</p>
+        <p className='text_footer'>Powered by Alvaro Sanchez & Jonathan Bolatti - Entregable final Front-end III</p>
         <img src="./images/DH.png" alt='DH-logo' />
-      <div>
-        <img src="./images/ico-facebook.png" alt="Facebook_icon" />
-        <img src="./images/ico-instagram.png" alt="instagram_icon" />
-        <img src="./images/ico-tiktok.png" alt="tiktok_icon" />
-        <img src="./images/ico-whatsapp.png" alt="whatsapp_icon"/>
-      </div>
     </footer>
   )
 }
